9f32b67d1ed01f9ca3e2afc6af79f8cf9cfe547f
MrRefactoring <MrRefactoring@yandex.ru>
2019-03-28 13:53:46 +0300

    dependencies updated

--- src/main.js
+++ src/main.js
@@ -10,7 +10,13 @@ const BrowserWindow = electron.BrowserWindow
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
